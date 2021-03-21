a76e22d682106ef317366064e8cbd7c69f441b55
Valentin Ramos <ramos.val.91@gmail.com>
2021-03-21 15:40:53 +0100

    Add FromString impl for Block object

--- src/block.rs
+++ src/block.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
 use std::fmt::{self, Debug, Formatter};
+use std::str::FromStr;
+use std::num::ParseIntError;
 
 /** Bloc: composants de la BlockChain
  * Composants d'un bloc
@@ -7,7 +10,7 @@ use std::fmt::{self, Debug, Formatter};
  * - Timestamp: pour avoir une notion de temps
  * - Nonce: nombre utilisé pour calculer le Proof of Work
  * - Previous block hash: L'empreinte cryptographique du bloc précédent
- * - Hash: l'empreinte cryptographique the toutes les données ci-dessus, concatanées ensemble
+ * - Hash: l'empreinte cryptographique de toutes les données ci-dessus, concatanées ensemble
  */
 
 pub struct Block {
@@ -32,13 +35,49 @@ impl Block {
     }
 }
 
-impl Debug for Block {
-    fn fmt (&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "Block[{}]: {} at: {} with: {}",
+impl fmt::Display for Block {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "id:{};prev_hash:{};timestamp:{};payload:{};nonce:{};hash:{}",
             &self.index,
-            &hex::encode(&self.hash),
+            &hex::encode(&self.prev_hash),
             &self.timestamp,
             &self.payload,
+            &self.nonce,
+            &hex::encode(&self.hash),
         )
+    }
+}
+
+impl Debug for Block {
+    fn fmt (&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "id:{};prev_hash:{};timestamp:{};payload:{};nonce:{};hash:{}",
+            &self.index,
+            &hex::encode(&self.prev_hash),
+            &self.timestamp,
+            &self.payload,
+            &self.nonce,
+            &hex::encode(&self.hash),
+        )
+    }
+}
+
+impl FromStr for Block {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let block: HashMap<String, String> = s.split(';')
+        .map(|kv| kv.split(':').collect::<Vec<&str>>())
+        .map(|vec| {
+            (vec[0].to_string(), vec[1].to_string())
+        })
+        .collect();
+        Ok(Block { 
+            index: block.get("id").unwrap().parse::<u32>().unwrap(), 
+            payload: block.get("payload").unwrap().to_string(),
+            timestamp: block.get("timestamp").unwrap().parse::<u64>().unwrap(), 
+            nonce: block.get("nonce").unwrap().parse::<u64>().unwrap(), 
+            prev_hash: block.get("prev_hash").unwrap().as_bytes().to_vec(),
+            hash:  block.get("hash").unwrap().as_bytes().to_vec(),
+     })
     }
 }
