de96ff3675ba7e25601d2b6048b676682e7baf6d
Valentin Ramos <ramos.val.91@gmail.com>
2021-03-21 17:43:16 +0100

    Add target compile files to ignore

--- target/rls/debug/deps/Blockchain-a97915c25a83a2ce.d
+++
@@ -1,6 +0,0 @@
-s:\Blockchain\Blockchain\target\rls\debug\deps\Blockchain-a97915c25a83a2ce.rmeta: src\main.rs src\miner.rs
-
-s:\Blockchain\Blockchain\target\rls\debug\deps\Blockchain-a97915c25a83a2ce.d: src\main.rs src\miner.rs
-
-src\main.rs:
-src\miner.rs:
